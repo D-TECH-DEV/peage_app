bc80373759a5d4c1745632b85b5c7ddf2a932232
Emmanuel Ble <Emmanuelble2004@gmail.com>
2026-04-21 18:18:11 +0000

    Creation des routes

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,45 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Importation de tous les contrôleurs
+use App\Http\Controllers\CategorieVehiculeController;
+use App\Http\Controllers\GuichetController;
+use App\Http\Controllers\PaiementController;
+use App\Http\Controllers\TarifController;
+use App\Http\Controllers\TypePaiementController;
+use App\Http\Controllers\UserController;
 
+
+
+// ==========================================
+// ROUTES FRONTEND (Utilisateur / Public)
+// ==========================================
+
+Route::get('/', function () {
+    return 'Bienvenue sur la page d\'accueil (Frontend) du Péage';
+});
+
+
+Route::get('/tarifs', [TarifController::class, 'index'])->name('frontend.tarifs.index');
+Route::get('/guichets', [GuichetController::class, 'index'])->name('frontend.guichets.index');
+
+
+
+// ==========================================
+// ROUTES BACKEND (Administration)
+// ==========================================
+Route::prefix('admin')->name('admin.')->group(function () {
+    
+    Route::get('/', function () {
+        return 'Tableau de bord de l\'Administration (Backend)';
+    })->name('dashboard');
+
+    // Routes complètes pour chaque entité (CRUD)
+    Route::resource('users', UserController::class);
+    Route::resource('categories-vehicules', CategorieVehiculeController::class);
+    Route::resource('guichets', GuichetController::class);
+    Route::resource('paiements', PaiementController::class);
+    Route::resource('tarifs', TarifController::class);
+    Route::resource('types-paiements', TypePaiementController::class);
+
+});
